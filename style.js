102e383267554b09dec5447b2886a0e70227ad42
Quocanh Kieu <kieuquocanh4@gmail.com>
2023-06-06 15:47:18 +0700

    ShopicClone

---
+++ style.js
@@ -0,0 +1,11 @@
+const home = document.querySelector(".homeContainer");
+console.log(home);
+
+let homeText = [
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+];
