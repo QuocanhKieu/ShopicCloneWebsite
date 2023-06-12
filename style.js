1b462cda503e94f3fc1bb6db9e1f421dbcfd9e87
QuocanhKieu <kieuquocanh4@gmail.com>
2023-06-12 11:18:34 +0700

    12/6

--- style.js
+++ style.js
@@ -1,9 +1,27 @@
-const home = document.querySelector(".homeContainer");
+const home = document.querySelector(".home.Container");
 const shop = document.querySelector(".shop");
 const page = document.querySelector(".home");
 console.log(home);
 
 let homeText = [
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
+  "Bicycle Shop",
+  "Tea Shop",
+  "Pet Shop",
   "Bicycle Shop",
   "Tea Shop",
   "Pet Shop",
@@ -17,6 +35,19 @@ homeText.forEach((item, index, arrAdd) => {
   redSquare.classList.add("red", "square");
 
   const anchor = document.createElement("a");
+  anchor.addEventListener("mouseover", () => {
+    redSquare.style.paddingInline = "0.25rem";
+    redSquare.style.backgroundColor = "#ff0000";
+    redSquare.style.marginRight = "1rem";
+    redSquare.style.marginLeft = "1rem";
+  });
+  anchor.addEventListener("mouseout", () => {
+    redSquare.style.paddingInline = "";
+    redSquare.style.backgroundColor = "";
+    redSquare.style.marginRight = "";
+    redSquare.style.marginLeft = "";
+  });
+
   anchor.textContent = item;
   anchor.setAttribute("href", "#");
 
